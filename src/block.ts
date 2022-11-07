d3f5f51ded367a10143f1cdcb0ea09a050e1f1fd
SudeepGowda55 <ssudeepgowda55@gmail.com>
2022-11-07 15:37:59 +0000

    first commit

--- src/block.ts
+++ src/block.ts
@@ -12,6 +12,7 @@ class Block {
        this.data = data;
     }
 }
+
 for (let number : number = 0; number < 10; number = number + 1 ) {
     const firstBlock = new Block(number, "0xabcfrs46483477rfhdcgdjgcj", "0xcdfsugdiiwt53442gghdi", "sudeep sent 1 million usd");
     const secondBlock = new Block(number + 1, `${firstBlock.currentBlockHash}`, "0xdshjdjgjdk", "sudeep sent 0.5 million usd")
