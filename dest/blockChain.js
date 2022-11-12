205ef3af8e3c14b78abafd3e5ff539ded6eaa290
SudeepGowda55 <ssudeepgowda55@gmail.com>
2022-11-12 19:13:40 +0000

    mining part done

--- dest/blockChain.js
+++ dest/blockChain.js
@@ -1,7 +1,9 @@
 import * as crypto from 'crypto';
+const hexToBinary = (hexData) => hexData.split('').map(i => parseInt(i, 16).toString(2).padStart(4, '0')).join('');
 const hashing = (...blockDetails) => {
-    return crypto.createHash('sha256').update(blockDetails.sort().join()).digest('hex');
+    return hexToBinary(crypto.createHash('sha256').update(blockDetails.sort().join("")).digest('hex'));
 };
+const miningRate = 10000; // Here the mining Rate is defined in milliseconds. so this is 10 seconds
 class Block {
     constructor(object) {
         this.timeStamp = object.timestamp;
@@ -12,18 +14,32 @@ class Block {
         this.difficulty = object.difficulty;
     }
     static genesis() {
-        return new Block({ timestamp: 1668181324864, prevBlockHash: '0x0000000000000000000000000000000000000000000000000000000000000000 ', currentBlockHash: '0x07166e335add1e3831a7a8fce006660067eca119108cc62122722e2feaaadc', data: "Genesis Block Created", nonce: 0, difficulty: 3 });
+        return new Block({ timestamp: 1668181324864, prevBlockHash: '0x0000000000000000000000000000000000000000000000000000000000000000 ', currentBlockHash: '0x07166e335add1e3831a7a8fce006660067eca119108cc62122722e2feaaadc', data: "Genesis Block Created", nonce: 0, difficulty: 15 });
     }
     static creatingBlock(miningData) {
         let nonceTimeStamp, currentHash, nonce = 0;
-        const { difficulty } = miningData.prevBlock;
+        let { difficulty } = miningData.prevBlock;
         const prevBlockHash = miningData.prevBlock.currentBlockHash;
         do {
             nonce++;
             nonceTimeStamp = Date.now();
+            difficulty = Block.adjustingDifficulty({ timestamp: nonceTimeStamp, originalBlock: miningData.prevBlock });
             currentHash = hashing(nonce, nonceTimeStamp, miningData.data, prevBlockHash, difficulty);
         } while (currentHash.substring(0, difficulty) !== '0'.repeat(difficulty));
-        return new this({ timestamp: nonceTimeStamp, prevBlockHash: prevBlockHash, currentBlockHash: currentHash, data: miningData.data, nonce: nonce, difficulty: difficulty });
+        console.log(difficulty);
+        return new this({ timestamp: nonceTimeStamp, prevBlockHash: prevBlockHash, currentBlockHash: parseInt(currentHash, 2).toString(16).toUpperCase(), data: miningData.data, nonce: nonce, difficulty: difficulty });
+    }
+    static adjustingDifficulty(difficultyObject) {
+        const { difficulty } = difficultyObject.originalBlock;
+        if (difficulty < 1)
+            return 1;
+        const differenceInTimestamp = difficultyObject.timestamp - difficultyObject.originalBlock.timeStamp;
+        if (differenceInTimestamp > miningRate) {
+            return difficulty - 1;
+        }
+        else {
+            return difficulty + 1;
+        }
     }
 }
 const genesisBlock = Block.genesis();
@@ -36,12 +52,12 @@ class BlockChain {
         this.chain.push(newBlock);
     }
     static validationCheck(newChain) {
-        if (JSON.stringify(newChain[0]) !== JSON.stringify(Block.genesis())) {
+        if (JSON.stringify(newChain.chain[0]) !== JSON.stringify(Block.genesis())) {
             console.error("Chain is not valid");
         }
-        for (let i = 1; i < newChain.length; i++) {
-            const { currentBlockHash, prevBlockHash, data, difficulty, nonce, timeStamp } = newChain[i];
-            const lastBlockHash = newChain[i - 1].currentBlockHash;
+        for (let i = 1; i < newChain.chain.length; i++) {
+            const { currentBlockHash, prevBlockHash, data, difficulty, nonce, timeStamp } = newChain.chain[i];
+            const lastBlockHash = newChain.chain[i - 1].currentBlockHash;
             if (prevBlockHash !== lastBlockHash)
                 return "Chain is not valid";
             const hashingFuncHash = hashing(timeStamp, data, difficulty, nonce, prevBlockHash);
@@ -51,7 +67,7 @@ class BlockChain {
         return "This Blockchain is valid";
     }
     selectingVerifiedChain(minerSentChain) {
-        if (minerSentChain.length <= this.chain.length) {
+        if (minerSentChain.chain.length <= this.chain.length) {
             console.error("This is not the longest Chain");
             return;
         }
@@ -60,14 +76,26 @@ class BlockChain {
             return;
         }
         else
-            this.chain = minerSentChain;
+            this.chain = minerSentChain.chain;
     }
 }
 const darkGuildBlockchain = new BlockChain();
-darkGuildBlockchain.addBlock({ data: "Sudeep Sent 1 million USD" });
+const timeStampDifferences = [];
+let prevBlockTimeStamp, nextBlockTimeStamp, timeDifference, avgTime;
+darkGuildBlockchain.addBlock({ data: "sudeep sent monisha 8 ETH" });
 darkGuildBlockchain.addBlock({ data: "monisha sent sudeep 10 ETH" });
-darkGuildBlockchain.addBlock({ data: " sudeep" });
-darkGuildBlockchain.addBlock({ data: "sh" });
+darkGuildBlockchain.addBlock({ data: "ms" });
+darkGuildBlockchain.addBlock({ data: "sudeep sent monisha 8 ETH" });
+darkGuildBlockchain.addBlock({ data: "monisha sent sudeep 10 ETH" });
+darkGuildBlockchain.addBlock({ data: "ms" });
+for (let j = 2; j < darkGuildBlockchain.chain.length; j++) {
+    prevBlockTimeStamp = darkGuildBlockchain.chain[j - 1].timeStamp;
+    nextBlockTimeStamp = darkGuildBlockchain.chain[j].timeStamp;
+    timeDifference = nextBlockTimeStamp - prevBlockTimeStamp;
+    timeStampDifferences.push(timeDifference);
+}
+avgTime = timeStampDifferences.reduce((total, num) => total + num) / timeStampDifferences.length;
+console.log(avgTime / 1000, "seconds");
 const verification = BlockChain.validationCheck(darkGuildBlockchain);
 if (verification) {
     console.log(darkGuildBlockchain);
--- src/blockChain.ts
+++ src/blockChain.ts
@@ -1,8 +1,12 @@
 import *  as crypto from 'crypto';
 
+const hexToBinary = (hexData : string) => hexData.split('').map(i => parseInt(i, 16).toString(2).padStart(4, '0')).join('');
+
 const hashing = (...blockDetails : any)  => {
-    return crypto.createHash('sha256').update(blockDetails.sort().join()).digest('hex');
+    return hexToBinary(crypto.createHash('sha256').update(blockDetails.sort().join("")).digest('hex'));
 }
+
+const miningRate : number = 10000; // Here the mining Rate is defined in milliseconds. so this is 10 seconds
 
 class Block {
 
@@ -23,19 +27,33 @@ class Block {
     }
 
     static genesis() : Block {
-        return new Block({timestamp: 1668181324864, prevBlockHash : '0x0000000000000000000000000000000000000000000000000000000000000000 ', currentBlockHash: '0x07166e335add1e3831a7a8fce006660067eca119108cc62122722e2feaaadc', data : "Genesis Block Created", nonce : 0, difficulty : 3})
+        return new Block({timestamp: 1668181324864, prevBlockHash : '0x0000000000000000000000000000000000000000000000000000000000000000 ', currentBlockHash: '0x07166e335add1e3831a7a8fce006660067eca119108cc62122722e2feaaadc', data : "Genesis Block Created", nonce : 0, difficulty : 15})
     }
 
     static creatingBlock(miningData : {prevBlock : Block, data : string}) {
         let nonceTimeStamp: number, currentHash : string, nonce : number = 0;
-        const { difficulty } = miningData.prevBlock ;
+        let { difficulty } = miningData.prevBlock ;
         const prevBlockHash : string = miningData.prevBlock.currentBlockHash;
         do {
             nonce ++; 
             nonceTimeStamp = Date.now();
+            difficulty = Block.adjustingDifficulty({timestamp : nonceTimeStamp, originalBlock : miningData.prevBlock});
             currentHash = hashing(nonce, nonceTimeStamp, miningData.data, prevBlockHash, difficulty );
         } while (currentHash.substring(0, difficulty) !== '0'.repeat(difficulty)) ;
-        return new this({timestamp : nonceTimeStamp, prevBlockHash : prevBlockHash, currentBlockHash: currentHash, data : miningData.data, nonce : nonce, difficulty : difficulty });
+        console.log(difficulty);
+        return new this({timestamp : nonceTimeStamp, prevBlockHash : prevBlockHash, currentBlockHash: parseInt(currentHash, 2).toString(16).toUpperCase(), data : miningData.data, nonce : nonce, difficulty : difficulty });
+    }   
+
+    static adjustingDifficulty(difficultyObject : {timestamp : number, originalBlock : Block}){
+        const {difficulty} = difficultyObject.originalBlock;
+        if (difficulty < 1) return 1;
+        const differenceInTimestamp : number = difficultyObject.timestamp - difficultyObject.originalBlock.timeStamp;
+        if (differenceInTimestamp > miningRate) {
+            return difficulty - 1;
+        } 
+        else {
+            return difficulty + 1;
+        }
     }
 }
 
@@ -48,19 +66,19 @@ class BlockChain {
         this.chain = [genesisBlock]
     }
 
-    addBlock(blockChainObject : {data : string, nonce : number, difficulty : number}) {
+    addBlock(blockChainObject : {data : string}) {
         const newBlock : Block = Block.creatingBlock({prevBlock : this.chain[(this.chain.length)-1], data : blockChainObject.data});
         this.chain.push(newBlock);
     }
 
-    static validationCheck (newChain : Block[])  {
-        // if (JSON.stringify(newChain[0]) !== JSON.stringify(Block.genesis())) {
-        //     console.error("Chain is not valid")
-        // }
+    static validationCheck (newChain : BlockChain)  {
+        if (JSON.stringify(newChain.chain[0]) !== JSON.stringify(Block.genesis())) {
+            console.error("Chain is not valid")
+        }
 
-        for (let i : number = 1; i < newChain.length; i ++ ){
-            const {currentBlockHash, prevBlockHash, data, difficulty, nonce, timeStamp} = newChain[i];
-            const lastBlockHash = newChain[i - 1].currentBlockHash;
+        for (let i : number = 1; i < newChain.chain.length; i ++ ){
+            const {currentBlockHash, prevBlockHash, data, difficulty, nonce, timeStamp} = newChain.chain[i];
+            const lastBlockHash = newChain.chain[i - 1].currentBlockHash;
             if (prevBlockHash !== lastBlockHash) return "Chain is not valid";
             const hashingFuncHash = hashing(timeStamp, data, difficulty, nonce, prevBlockHash);
             if (currentBlockHash !== hashingFuncHash) return "Chain is not valid";
@@ -69,8 +87,8 @@ class BlockChain {
         return "This Blockchain is valid"
     }
 
-    selectingVerifiedChain (minerSentChain : Block[]) {
-      if (minerSentChain.length <= this.chain.length){
+    selectingVerifiedChain (minerSentChain : BlockChain) {
+      if (minerSentChain.chain.length <= this.chain.length){
         console.error("This is not the longest Chain");
         return
       }
@@ -79,16 +97,33 @@ class BlockChain {
         console.error("This Chain is not valid")
         return
       }
-      else this.chain = minerSentChain;
+      else this.chain = minerSentChain.chain
     }
 }
 
-const darkGuildBlockchain : any = new BlockChain();
+const darkGuildBlockchain : BlockChain  = new BlockChain();
 
-darkGuildBlockchain.addBlock({data: "Sudeep Sent 1 million USD"});
+const timeStampDifferences : number[] = [];
+
+let prevBlockTimeStamp : number, nextBlockTimeStamp : number, timeDifference : number, avgTime : number ;
+
+darkGuildBlockchain.addBlock({data : "sudeep sent monisha 8 ETH"});
 darkGuildBlockchain.addBlock({data : "monisha sent sudeep 10 ETH"});
-darkGuildBlockchain.addBlock({data : " sudeep"});
-darkGuildBlockchain.addBlock({data : "sh"});
+darkGuildBlockchain.addBlock({data : "ms"});
+darkGuildBlockchain.addBlock({data : "sudeep sent monisha 8 ETH"});
+darkGuildBlockchain.addBlock({data : "monisha sent sudeep 10 ETH"});
+darkGuildBlockchain.addBlock({data : "ms"});
+
+for (let j = 2 ; j < darkGuildBlockchain.chain.length; j ++) {
+    prevBlockTimeStamp = darkGuildBlockchain.chain[j- 1].timeStamp;
+    nextBlockTimeStamp = darkGuildBlockchain.chain[j].timeStamp;
+    timeDifference = nextBlockTimeStamp - prevBlockTimeStamp;
+    timeStampDifferences.push(timeDifference);
+}
+
+avgTime = timeStampDifferences.reduce((total : number, num : number) => total + num) /timeStampDifferences.length;
+
+console.log(avgTime / 1000, "seconds");
 
 const verification = BlockChain.validationCheck(darkGuildBlockchain);
 
